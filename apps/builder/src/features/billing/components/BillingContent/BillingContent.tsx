888ad091fbf52804105f1508e7792dc8fff75490
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-12-20 21:17:54 -0500

    Update BillingContent.tsx

--- apps/builder/src/features/billing/components/BillingContent/BillingContent.tsx
+++ apps/builder/src/features/billing/components/BillingContent/BillingContent.tsx
@@ -1,12 +1,10 @@
-import { HStack, Stack, Text } from '@chakra-ui/react'
 import { useWorkspace } from '@/features/workspace'
 import { Plan } from 'db'
 import React from 'react'
 import { CurrentSubscriptionContent } from './CurrentSubscriptionContent'
 import { InvoicesList } from './InvoicesList'
 import { UsageContent } from './UsageContent/UsageContent'
-import { StripeClimateLogo } from '../StripeClimateLogo'
-import { TextLink } from '@/components/TextLink'
+
 import { ChangePlanForm } from '../ChangePlanForm'
 
 export const BillingContent = () => {
@@ -23,14 +21,7 @@ export const BillingContent = () => {
           onCancelSuccess={refreshWorkspace}
         />
         <HStack maxW="500px">
-          <StripeClimateLogo />
-          <Text fontSize="xs" color="gray.500">
-            Typebot is contributing 1% of your subscription to remove CO₂ from
-            the atmosphere.{' '}
-            <TextLink href="https://climate.stripe.com/5VCRAq" isExternal>
-              More info.
-            </TextLink>
-          </Text>
+        
         </HStack>
         {workspace.plan !== Plan.CUSTOM &&
           workspace.plan !== Plan.LIFETIME &&
@@ -41,3 +32,4 @@ export const BillingContent = () => {
     </Stack>
   )
 }
+
