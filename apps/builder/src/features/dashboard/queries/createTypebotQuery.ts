9244a9b69ee03c1a2d1122649d080b29dd59c69c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-12-22 11:20:54 -0500

    Update createTypebotQuery.ts

--- apps/builder/src/features/dashboard/queries/createTypebotQuery.ts
+++ apps/builder/src/features/dashboard/queries/createTypebotQuery.ts
@@ -7,7 +7,7 @@ export const createTypebotQuery = async ({
 }: Pick<Typebot, 'folderId' | 'workspaceId'>) => {
   const typebot = {
     folderId,
-    name: 'My typebot',
+    name: 'My liisk',
     workspaceId,
   }
   return sendRequest<Typebot>({
